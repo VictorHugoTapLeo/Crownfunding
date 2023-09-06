b3e8f4d980c8224ecd212c187707c2a9832242f0
VictorHugoTapLeo <tlv2018944@est.univalle.edu>
2023-09-06 19:59:13 -0400

    subida del proyecto v2

---
+++ Crownfunding Proyecto/Avanze_ProjectoWeb/Avanze_ProjectoWeb/Models/PaginaUnoModeldos.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace Avanze_ProjectoWeb.Models
+{
+    public class PaginaUnoModeldos
+    {
+        public string? Titulo { get; set; }
+
+        [Required(ErrorMessage = "El campo DescripcionGeneral es obligatorio.")]
+        public string? DescripcionGeneral { get; set; }
+
+        [Required(ErrorMessage = "Debes seleccionar al menos un apoyo requerido.")]
+        public List<string> ListaApoyos { get; set; }
+
+        public string ComentarioMision { get; set; }
+        public string ComentarioVision { get; set; }
+        public string ComentarioSobre { get; set; }
+
+        [Required(ErrorMessage = "Debes cargar una imagen para el proyecto.")]
+        public byte[] ImagenProyecto { get; set; }
+
+        [Required(ErrorMessage = "Debes cargar una imagen del producto final.")]
+        public byte[] ImagenProductoFinal { get; set; }
+
+        [Required(ErrorMessage = "Debes cargar una imagen del proceso de producción.")]
+        public byte[] ImagenProcesoProduccion { get; set; }
+
+        [Required(ErrorMessage = "El campo Link es obligatorio.")]
+        [RegularExpression(@"^(https?://)?(www\.)?youtube\.com/embed/[^/\s]+$", ErrorMessage = "El formato del enlace de video no es válido.")]
+        public string Link { get; set; }
+
+        [Required(ErrorMessage = "El campo Redes es obligatorio.")]
+        public string Redes { get; set; }
+
+        [Required(ErrorMessage = "Debes seleccionar una categoría.")]
+        public string Tipo { get; set; }
+
+    }
+}
