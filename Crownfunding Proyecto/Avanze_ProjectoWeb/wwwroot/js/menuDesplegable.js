59939a9bef15a619b1de690bd787eabc7601fc77
VictorHugoTapLeo <tlv2018944@est.univalle.edu>
2023-10-24 23:47:47 -0400

    layout y menu desplegable

---
+++ Crownfunding Proyecto/Avanze_ProjectoWeb/wwwroot/js/menuDesplegable.js
@@ -0,0 +1,24 @@
+﻿// script.js
+
+
+
+
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        document.removeEventListener('click', closeMenuOutsideClick);
+    } else {
+        menu.classList.add('active');
+        document.addEventListener('click', closeMenuOutsideClick);
+    }
+}
+
+function closeMenuOutsideClick(event) {
+    var menu = document.getElementById('menu');
+    var button = document.querySelector('.menu-button');
+    if (menu.classList.contains('active') && event.target !== menu && event.target !== button) {
+        menu.classList.remove('active');
+        document.removeEventListener('click', closeMenuOutsideClick);
+    }
+}
